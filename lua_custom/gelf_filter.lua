6827d1a8a0197acd65181af58e78ae7fddecfea3
rohit01 <hello@rohit.io>
2016-07-18 17:18:37 +0530

    Adding space

--- lua_custom/gelf_filter.lua
+++ lua_custom/gelf_filter.lua
@@ -28,7 +28,7 @@ end
 function process_message()
     logger = read_message("Logger")
     gelf_json = gelf_format(read_message("Payload"), logger)
-    if gelf_json then
+    if gelf_json then 
         local ok, message = pcall(cjson.encode, gelf_json)
         if not ok then 
             return nil
@@ -42,10 +42,10 @@ end
 
 
 function validated_gelf_value(value)
-    if not (type(value) == 'string') then
+    if not (type(value) == 'string') then 
         value = cjson.encode(value)
     end
-    if value and (string.len(value) > max_field_len) then
+    if value and (string.len(value) > max_field_len) then 
         value = string.sub(value, 1, trunc_msg_len) .. "\n...truncated"
     end
     return value
@@ -55,15 +55,15 @@ end
 function gelf_format(payload, logger)
     local gelfentry = {}
     gelfentry['timestamp'] = os.time()
-    if logger then
-        if string.sub(logger, 1, string.len('container_id.')) == 'container_id.' then
+    if logger then 
+        if string.sub(logger, 1, string.len('container_id.')) == 'container_id.' then 
             gelfentry['_container_id'] = string.sub(logger, string.len('container_id.') + 1, -1)
         end
     end
 
     local json_msg
     local ok
-    if type(payload) == 'table' then
+    if type(payload) == 'table' then 
         json_msg = payload
     else
         ok, json_msg = pcall(cjson.decode, payload)
@@ -74,7 +74,7 @@ function gelf_format(payload, logger)
     end
 
     for key, value in pairs(json_msg) do
-        if (key == 'attrs') and (type(value) == 'table') then
+        if (key == 'attrs') and (type(value) == 'table') then 
             for k, v in pairs(value) do
                 gelfentry['_' .. k] = validated_gelf_value(v)
             end
@@ -82,7 +82,7 @@ function gelf_format(payload, logger)
             gelfentry['_' .. key] = validated_gelf_value(value)
         end
     end
-    if not gelfentry['short_message'] then
+    if not gelfentry['short_message'] then 
         gelfentry['short_message'] = string.sub(payload, 1, short_field_len)
     end
 
